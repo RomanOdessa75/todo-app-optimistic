3476472e573bad4c9cf264ea5627ecc2a6c6016b
RomanOdessa75 <115027650+RomanOdessa75@users.noreply.github.com>
2025-04-03 01:40:59 +0300

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  basePath: "/todo-app-optimistic",
-  assetPrefix: "/todo-app-optimistic/",
 };
 
 module.exports = nextConfig;
